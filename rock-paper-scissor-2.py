419bfe6603b411d31f806985cbffffb80a19d172
ttorble <v-trtorb@microsoft.com>
2022-05-06 11:35:15 +0100

    fix indents in code

--- rock-paper-scissor-2.py
+++ rock-paper-scissor-2.py
@@ -1,62 +1,62 @@
 class Participant:
-  def __init__(self, name):
-    self.name = name
-    self.points = 0
-    self.choice = ""
-  def choose(self):
-    self.choice = input("{name}, select rock, paper or scissor: ".format(name= self.name))
-    print("{name} selects {choice}".format(name=self.name, choice = self.choice))
-  def toNumericalChoice(self):
-    switcher = {
-      "rock": 0,
-      "paper": 1,
-      "scissor": 2 
-    }
-    return switcher[self.choice]
-  def incrementPoint(self):
-    self.points += 1 
+    def __init__(self, name):
+        self.name = name
+        self.points = 0
+        self.choice = ""
+    def choose(self):
+        self.choice = input("{name}, select rock, paper or scissor: ".format(name= self.name))
+        print("{name} selects {choice}".format(name=self.name, choice = self.choice))
+    def toNumericalChoice(self):
+        switcher = {
+            "rock": 0,
+            "paper": 1,
+            "scissor": 2 
+        }
+        return switcher[self.choice]
+    def incrementPoint(self):
+        self.points += 1 
 
 class GameRound:
-  def __init__(self, p1, p2):
-    self.rules = [
-      [0, -1, 1],
-      [1, 0, -1],
-      [-1, 1, 0]
-    ] 
+    def __init__(self, p1, p2):
+        self.rules = [
+            [0, -1, 1],
+            [1, 0, -1],
+            [-1, 1, 0]
+        ] 
 
-    p1.choose()
-    p2.choose()
-    result = self.compareChoices(p1,p2)
-    print("Round resulted in a {result}".format(result = self.getResultAsString(result) ))
-    if result > 0:
-      p1.incrementPoint()
-    elif result < 0:
-      p2.incrementPoint()
+        p1.choose()
+        p2.choose()
+        result = self.compareChoices(p1,p2)
+        print("Round resulted in a {result}".format(result = self.getResultAsString(result) ))
+        if result > 0:
+            p1.incrementPoint()
+        elif result < 0:
+            p2.incrementPoint()
     
-  def compareChoices(self, p1, p2):
-    return self.rules[p1.toNumericalChoice()][p2.toNumericalChoice()]
-  def awardPoints(self):
-    print("implement")
-  def getResultAsString(self, result):
-    res = {
-      0: "draw",
-      1: "win",
-      -1: "loss"
-    }
-    return res[result]
+    def compareChoices(self, p1, p2):
+        return self.rules[p1.toNumericalChoice()][p2.toNumericalChoice()]
+    def awardPoints(self):
+        print("implement")
+    def getResultAsString(self, result):
+        res = {
+            0: "draw",
+            1: "win",
+            -1: "loss"
+        }
+        return res[result]
 
 class Game:
-  def __init__(self):
-    self.endGame = False
-    self.participant = Participant("Spock")
-    self.secondParticipant = Participant("Kirk")
-  def start(self):
-    game_round = GameRound(self.participant, self.secondParticipant)
+    def __init__(self):
+        self.endGame = False
+        self.participant = Participant("Spock")
+        self.secondParticipant = Participant("Kirk")
+    def start(self):
+        game_round = GameRound(self.participant, self.secondParticipant)
 
-  def checkEndCondition(self):
-    print("implement")
-  def determineWinner(self):
-    print("implement")
+    def checkEndCondition(self):
+        print("implement")
+    def determineWinner(self):
+        print("implement")
 
 game = Game()
 game.start()
